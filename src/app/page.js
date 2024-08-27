ee410a7b89372c6c703b32a4fe702b8ed0e7545b
NaiyaGo <909424998@qq.com>
2024-08-28 01:16:45 +0800

    as

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,30 @@ import ToolGrid from "@/components/ToolGrid";
 export default async function Home({searchParams}) {
 
   //console.log(await supabase.from('card').select('*,category!inner(*)').eq('category.cate_name','Just-Share'));
-  
+  console.log('cwd=',process.cwd());
+  function findFile(dirPath) {
+    try {
+        // 读取目录中的内容
+        const items = fs.readdirSync(dirPath);
+        
+        for (const item of items) {
+            const itemPath = path.join(dirPath, item);
+            const stats = fs.statSync(itemPath);
+
+            if (stats.isDirectory()) {
+                // 如果是目录，则递归查找
+                findFile(itemPath);
+            } else if (item === targetFileName) {
+                // 如果找到指定文件，输出其路径
+                console.log(`找到文件: ${itemPath}`);
+            }
+        }
+    } catch (error) {
+        // 如果无法访问目录，则打印错误信息
+        console.error(`无法访问: ${dirPath}`);
+    } 
+  }
+  findFile('fileposData.json');
   return (
 
         <div className="flex">
