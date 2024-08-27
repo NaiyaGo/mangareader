6c4130542cd3094bd225fd61e6eaa2e02b27a8a4
NaiyaGo <909424998@qq.com>
2024-08-28 00:30:40 +0800

    修改路径

--- src/app/category/[slug]/page.js
+++ src/app/category/[slug]/page.js
@@ -15,7 +15,7 @@ import {promises as fs} from 'fs';
 //   }
 
   export async function generateStaticParams() {
-    const file=await fs.readFile('/SidebarData.json','utf-8');
+    const file=await fs.readFile(process.cwd() + '/app/SidebarData.json', 'utf8');
     const categoryData=JSON.parse(file);
 
     return categoryData.map((category) => {
