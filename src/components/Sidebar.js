65c54d86e0ebd05675043210b11cd903a6441375
NaiyaGo <909424998@qq.com>
2024-08-28 13:24:24 +0800

    修改build和runtime路径处理

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,14 +7,34 @@ import {promises as fs} from 'fs';
 export async function getCategories() {
   "use server"
   console.log('Looking for file at:', process.cwd()+'/public/SidebarData.json');
-  const file=await fs.readFile(process.cwd()+'/public/SidebarData.json', 'utf8');
-  const categoryData=JSON.parse(file);
+  const filePath=process.cwd()+'/public/SidebarData.json';
+  if(fs.existsSync(filePath)){
+    console.log('file exists');
+    const file=await fs.readFile( process.cwd()+'/public/SidebarData.json', 'utf8');
+    const categoryData=JSON.parse(file);
 
   return categoryData.map((category) => {
     return {
-        cate_name:category.cate_name,
+        slug: category.cate_name,
       };
   });
+  }else{
+    console.log('Detect Runtime phase:', 'use fetch instead');
+    try {
+      const response = await fetch('/SidebarData.json'); // 确保文件在 public 目录
+      if (!response.ok) throw new Error('Network response was not ok');
+      const data = await response.json();
+      const categoryData=data.map((category) => {
+        return {
+            slug: data.cate_name,
+          };
+      });
+      return categoryData;
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return [];
+    }
+  }
 }
 const Sidebar = async () => {
   
